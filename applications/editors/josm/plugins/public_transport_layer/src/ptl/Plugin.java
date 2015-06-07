c8cd3a2ecd0492dbd4721f4cac13710488089863
don-vip <vincent.privat@gmail.com>
2015-06-07 22:36:23 +0000

    [josm_public_transport_layer] finish tree setup. Especially creates the required "data" folder that makes the i18n task fail after 45 minutes :(

---
+++ applications/editors/josm/plugins/public_transport_layer/src/ptl/Plugin.java
@@ -0,0 +1,14 @@
+package ptl;
+
+import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.gui.MainMenu;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class Plugin extends org.openstreetmap.josm.plugins.Plugin {
+
+    public Plugin(PluginInformation info) {
+        super(info);
+        MainMenu.add(Main.main.menu.dataMenu, new PublicTransportLayer.AddLayerAction());
+    }
+}
+
